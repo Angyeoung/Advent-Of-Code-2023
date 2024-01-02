b14b72e307d0321cea138fe3ceaebef8e1fb7d90
Angyeoung <realandreyb@gmail.com>
2024-01-02 15:30:06 -0800

    completed day 2 part 2

--- 2/solution.py
+++ 2/solution.py
@@ -1,6 +1,6 @@
 import re
 
-# Returns 0 if the game is impossible, or the ID of the game if possible
+# Part 1
 def game_is_possible(line = ""):
     max_red = 12
     max_green = 13
@@ -27,5 +27,38 @@ total = 0
 with open(INPUT, "r") as file:
     for line in file:
         total += game_is_possible(line)
+print(total)
+
+
+# Part 2 
+def power_of_game(line):
+    min_colors = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+
+    game_info = re.sub("Game.+: ", "", line).rstrip()
+    game_parts = re.split(";", game_info)
+    
+    for game_part in game_parts:
+        colors_revealed = re.split(",", game_part)
+        for color_revealed in colors_revealed:
+            number = int(re.search("\d+", color_revealed)[0])
+            color = "red"
+            if "blue" in color_revealed:
+                color = "blue"
+            elif "green" in color_revealed: 
+                color = "green"
+            
+            if number > min_colors[color]:
+                min_colors[color] = number
+    return min_colors["red"] * min_colors["green"] * min_colors["blue"]
+
+INPUT = "input.txt"
+total = 0
+with open(INPUT, "r") as file:
+    for line in file:
+        total += power_of_game(line)
 print(total)
 
