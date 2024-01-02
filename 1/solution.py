f650589bac354fe7e92237e83519cf4a89f51306
Angyeoung <realandreyb@gmail.com>
2024-01-02 14:22:41 -0800

    solved day 1 part 2

--- 1/solution.py
+++ 1/solution.py
@@ -1,8 +1,7 @@
-"""
-Part One
-"""
+INPUT = "input.txt"
+# Part One
 total = 0
-with open("input.txt", "r") as file:
+with open(INPUT, "r") as file:
     for line in file:
         first = last = None
         for char in line:
@@ -13,6 +12,37 @@ with open("input.txt", "r") as file:
         total += int(first + last)
 print(total)
 
-"""
-Part Two
-"""
+
+# Part Two
+digits = {
+    "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6",
+    "7": "7", "8": "8", "9": "9",
+    "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", 
+    "six": "6", "seven": "7", "eight": "8", "nine": "9"
+}
+
+
+total = 0
+with open(INPUT, "r") as file:
+    for line in file:
+        firstDigit = lastDigit = None
+        firstIndex = lastIndex = -1
+        for [string, value] in digits.items():
+            index = line.find(string)
+            rindex = line.rfind(string)
+            if index == -1 and rindex == -1:
+                continue
+            if firstDigit is None or index < firstIndex:
+                firstDigit = string
+                firstIndex = index
+            if lastDigit is None or index > lastIndex:
+                lastDigit = string
+                lastIndex = index
+            if firstDigit is None or rindex < firstIndex:
+                firstDigit = string
+                firstIndex = rindex
+            if lastDigit is None or rindex > lastIndex:
+                lastDigit = string
+                lastIndex = rindex
+        total += int(digits[firstDigit] + digits[lastDigit])
+print(total)
