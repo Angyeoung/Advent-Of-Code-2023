ba0e7e5ca592a2970785a3bc37834f2750eb3983
Angyeoung <realandreyb@gmail.com>
2024-01-18 21:36:35 -0800

    Finished day 1 part 1

---
+++ 5/solution.py
@@ -0,0 +1,14 @@
+# Part 1
+with open("input.txt") as file:
+    seeds = [int(x) for x in file.readline()[6:].split()]
+    check = [False for _ in seeds]
+    for line in file.readlines():
+        if not line[0].isdigit(): 
+            check = [False for _ in seeds]; continue
+        [dest, source, length] = [int(x) for x in line.split(" ")]
+        for i, seed in enumerate(seeds):
+            if check[i]: continue
+            if not source <= seed < source + length: continue
+            seeds[i] += dest - source
+            check[i] = True
+    print(min(seeds))
