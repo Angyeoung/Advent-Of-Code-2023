4f7fbdfd7e39f0e1eaf4e5e5425352e11e9cb1b9
Angyeoung <realandreyb@gmail.com>
2024-01-31 19:49:02 -0800

    Created outline for implementation, time for zzz amirmirmir zzz

--- 7/solution.py
+++ 7/solution.py
@@ -12,8 +12,41 @@ for card in hand:
 
 
 """
-    Find a way to actually calculate a winner,
-    Find a way to store the rankings
-    Find a way to sort the rankings and/or place them in order
+    Points system?:
+        J = 11, Q = 12, K = 13, A = 1
+        AAAAA = 1414141414
+        22222 = 0202020202
+        Should work for all hands and allow easy comparisons (Higher number wins)
+        Could possible add incrememnts of 10 billion to denote hand rank too,
+        grouping the entire hand score into one number
+
+    If two hands have the same type, a second ordering rule takes effect. 
+    Start by comparing the first card in each hand. If these cards are different, 
+    the hand with the stronger first card is considered stronger. If the first 
+    card in each hand have the same label, however, then move on to considering 
+    the second card in each hand. If they differ, the hand with the higher 
+    second card wins; otherwise, continue with the third card in each hand, 
+    then the fourth, then the fifth.
+
+    1. Use card counts to classify the type of hand
+        Five of a kind:
+            { A: 5 }
+        Four of a kind:
+            { A: 4, K: 1}
+        Full house:
+            { A: 3, K: 2}
+        Three of a kind:
+            { A: 3, K: 1, Q: 1 }
+        Two pair:
+            { A: 2, K: 2, Q: 1 }
+        One pair:
+            { A: 2, K: 1, Q: 1, J: 1 }
+        High Card:
+            { A: 1, K: 1, Q: 1, J: 1, T: 1 }
+
+    2. Use point system to classify card score (Read above)
+    3. Efficiently sort the hands by their score and give them their ranks
+    4. Do the final math to calculate result 
+        
 
 """
