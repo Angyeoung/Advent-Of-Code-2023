0779463d4a77b87e8ddd2a99a837afcfd419c189
Angyeoung <realandreyb@gmail.com>
2024-02-01 21:54:39 -0800

    It ain't much but it's honest work

--- 7/solution.py
+++ 7/solution.py
@@ -2,20 +2,34 @@
 with open("input.txt") as file:
     lines = [line.split() for line in file.readlines()]
 
-# To get the count
-[hand, bid] = lines[0]
-counts = {}
-for card in hand:
-    if counts.get(card) != None:
-        counts[card] = hand.count(card)
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+for line in lines:
+    [hand, bid] = line
+    cardValue = 0
+    # Get counts
+    counts = {}
+    for card in hand:
+        if counts.get(card) == None:
+            counts[card] = hand.count(card)
+    
+    # Add points for hand rank
+    vals = list(counts.values())
+    if 5 in vals: cardValue = 60000000000
+    elif 4 in vals: cardValue = 50000000000
+    elif 3 in vals and 2 in vals: cardValue = 40000000000
+    elif 3 in vals: cardValue = 30000000000
+    elif vals.count(2) == 2: cardValue = 20000000000
+    elif 2 in vals: cardValue = 10000000000
 
+    # Add points for cards
+    cardValue += int("".join([str(cards.index(c)).ljust(2, "0") for c in hand]))
 
 
 """
     Points system?:
         J = 11, Q = 12, K = 13, A = 1
-        AAAAA = 1414141414
-        22222 = 0202020202
+        AAAAA = 1212121212
+        22222 = 0000000000
         Should work for all hands and allow easy comparisons (Higher number wins)
         Could possible add incrememnts of 10 billion to denote hand rank too,
         grouping the entire hand score into one number
